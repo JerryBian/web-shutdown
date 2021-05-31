bef8ac6673b1cd6223b8e4f4c60b3e170a39213b
JerryBian <JerryBian@outlook.com>
2021-05-31 15:59:58 +0800

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, send_from_directory, jsonify
+import subprocess
+import sys
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def index():
+    return send_from_directory('', 'index.html')
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    command = subprocess.run(['sudo', 'shutdown'], capture_output=True)
+    return jsonify({'stdout': command.stdout.decode(), 'stderr': command.stderr.decode()})
+
+@app.route('/reboot', methods=['POST'])
+def shutdown():
+    command = subprocess.run(['sudo', 'reboot'], capture_output=True)
+    return jsonify({'stdout': command.stdout.decode(), 'stderr': command.stderr.decode()})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
