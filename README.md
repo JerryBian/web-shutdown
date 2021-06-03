afb73ab2d2766f47c7f1a3cafcaccd4ccd778843
JerryBian <JerryBian@outlook.com>
2021-06-03 14:08:01 +0800

    redign the output

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This small utility helps to shutdown your host via web UI interface.
 
-This is only tested in Ubuntu, other platforms might need to tweak a little bit, but it should be quite easy.
+It is only tested in Ubuntu, other platforms might need to tweak a little bit, but it should be quite easy.
 
 ### Screenshot
 
@@ -20,13 +20,14 @@ This is only tested in Ubuntu, other platforms might need to tweak a little bit,
 3. Create Systemd unit file
    ```
    python3_bin=$(which python3)
+
    echo "
    [Unit]
    Description=shutdown via web
    After=network.target
 
    [Service]
-   User=$USER
+   User=root
    WorkingDirectory=/tmp/web-shutdown
    Restart=on-failure
    RestartSec=5s
@@ -35,6 +36,7 @@ This is only tested in Ubuntu, other platforms might need to tweak a little bit,
    [Install]
    WantedBy=multi-user.target
     " > /etc/systemd/system/web-shutdown.service
+
    systemctl enable web-shutdown
    systemctl start web-shutdown
    ```
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory, jsonify
+from threading import Thread
 import subprocess
-import sys
+import traceback
+import logging
+import os
 
 app = Flask(__name__, static_url_path='')
 
@@ -11,14 +14,33 @@ def index():
 
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
-    command = subprocess.run(['sudo', 'shutdown', '-h', 'now'], capture_output=True)
-    return jsonify({'stdout': command.stdout.decode(), 'stderr': command.stderr.decode()})
+    out = err = str()
+    try:
+        args = ['sudo', 'shutdown', '-H', 'now']
+        exeCmd(args)
+        out = 'machine has been shutdown successfully'
+    except Exception:
+        err = traceback.format_exc()
+        logging.error(err)
+    return jsonify({'stdout': out, 'stderr': err})
 
 @app.route('/reboot', methods=['POST'])
 def reboot():
-    command = subprocess.run(['sudo', 'reboot'], capture_output=True)
-    return jsonify({'stdout': command.stdout.decode(), 'stderr': command.stderr.decode()})
+    out = err = str()
+    try:
+        args = ['sudo', 'shutdown', '-r', 'now']
+        exeCmd(args)
+        out = 'machine has been reboot successfully'
+    except Exception:
+        err = traceback.format_exc()
+        logging.error(err)
+    return jsonify({'stdout': out, 'stderr': err})
 
+def exeCmd(args):
+    command = subprocess.run(args, capture_output=True)
+    thread = Thread(target = lambda: subprocess.run(args, capture_output=True))
+    thread.start()
+    return (thread, command)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host=os.getenv('ENV_HOST', '127.0.0.1'), port=os.getenv('ENV_PORT', '5000'))
