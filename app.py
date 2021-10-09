3ae59351ce1cb6c1ae20842ddefa47be24a4deca
JerryBian <JerryBian@outlook.com>
2021-10-09 10:44:57 +0800

    enhance style

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def shutdown():
     try:
         args = ['sudo', 'shutdown', '-h', 'now']
         executor.submit(exeCmd, args)
-        out = f'Machine(<small class="text-muted fs-6">{get_machine()}</small>) has been shutdown successfully'
+        out = f'Machine <small class="text-muted fs-6 ps-1 pe-1">({get_machine()})</small> has been shutdown successfully'
     except Exception:
         err = traceback.format_exc()
         logger.error(err)
@@ -106,7 +106,7 @@ def reboot():
     try:
         args = ['sudo', 'shutdown', '-r', 'now']
         executor.submit(exeCmd, args)
-        out = f'Machine(<small class="text-muted fs-6">{get_machine()}</small>) has been reboot successfully'
+        out = f'Machine <small class="text-muted fs-6 ps-1 pe-1">({get_machine()})</small> has been reboot successfully'
     except Exception:
         err = traceback.format_exc()
         logger.error(err)
